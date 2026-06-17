e0ef92d3fccc12035b87505a8a52624a5bcbec84
Jan Tomaka <jan@tomaka.it>
2026-06-17 18:01:12 +0200

    wrapped in function, removed unnecesary \n branch, added flush at the end to make sure last character doesn't get omitted

--- lexer.rb
+++ lexer.rb
@@ -1,41 +1,37 @@
-print "input your expr: "
-input = gets
-tokenized_expression = []
-flush = ->(x) { tokenized_expression << x unless x.empty? ; ""}
-star_op = ""
-current_number = ""
-mystery_chars = ""
+def lex(input)
+  tokenized_expression = []
+  flush = ->(x) { tokenized_expression << x unless x.empty? ; ""}
+  star_op = ""
+  current_number = ""
+  mystery_chars = ""
 
+  input.each_char do |c|
+    if c == " "
+      current_number = flush.call(current_number)
+      star_op = flush.call(star_op)
 
+    elsif "0123456789.".include?(c)
+      star_op = flush.call(star_op)
+      current_number << c
 
+    elsif "+-/()".include?(c)
+      current_number = flush.call(current_number)
+      star_op = flush.call(star_op)
+      tokenized_expression << c
 
-input.each_char do |c|
-  if c == " "
-    current_number = flush.call(current_number)
-    star_op = flush.call(star_op)
+    elsif c == "*"
+      star_op << "*"
+      current_number = flush.call(current_number)
 
-  elsif c == "\n"
-    current_number = flush.call(current_number)
-    star_op = flush.call(star_op)
-    
-  elsif "0123456789.".include?(c)
-    star_op = flush.call(star_op)
-    current_number << c
+    else
+      mystery_chars << (c)
+      puts "tf is this: #{mystery_chars}"
 
-  elsif "+-/()".include?(c)
-    current_number = flush.call(current_number)
-    star_op = flush.call(star_op)
-    tokenized_expression << c
-
-  elsif c == "*"
-    star_op << "*"
-    current_number = flush.call(current_number)
-
-  else
-    mystery_chars << (c)
-    puts "tf is this: #{mystery_chars}"
-
+    end
   end
-end
 
-print tokenized_expression
+  current_number = flush.call(current_number)
+  star_op = flush.call(star_op)
+      
+  tokenized_expression
+end
