fb9e1d5efe244b7f160c9da4dd09d82bac0ae2e9
Jan Tomaka <jan@tomaka.it>
2026-06-18 13:35:56 +0200

    removed unnecessary depth tracking

--- main.rb
+++ main.rb
@@ -1,15 +1,13 @@
 def parse
   require_relative 'lexer'
   expression = Lexer.lex("2^2*3(2^2)")
-  paren_depth = 0
   paren_indices = []
 
 
   expression.each_with_index do |token, i| case token when "("
-      paren_depth+=1 #increment depth upon left paren encounter
       paren_indices << i #save the index to make life easier for eval
 
-    when ")"  # find matching l paren upon each r paren encounter, eval, decrement depth
+    when ")"  # find matching l paren upon each r paren encounter, eval
       
       frag = expression[paren_indices.last+1 .. i-1]
       expression[paren_indices.last .. i] = [compute(frag)]
