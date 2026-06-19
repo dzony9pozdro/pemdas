26e36f0aa2e25e11023718ddff779c45af931e8e
Jan Tomaka <jan@tomaka.it>
2026-06-19 17:16:34 +0200

    got rid of the janky evaluator, made validator, some lexer corrections

--- lexer.rb
+++ lexer.rb
@@ -3,27 +3,23 @@ module Lexer
     tokenized_expression = []
     flush = ->(x) { tokenized_expression << x unless x.empty? ; ""}
     current_number = ""
-    mystery_chars = ""
 
     input.each_char do |c|
+
       if c == " "
         current_number = flush.call(current_number)
 
       elsif "0123456789.".include?(c)
         current_number << c
 
-      elsif "+-/()^*".include?(c)
+      else "+-/()^*".include?(c)
         current_number = flush.call(current_number)
         tokenized_expression << c
-
-      else
-        mystery_chars << (c)
       end
     end
 
-    current_number = flush.call(current_number)
+    flush.call(current_number)
         
-    puts "tf is this: #{mystery_chars}" unless mystery_chars.empty?
     tokenized_expression
   end
 end
